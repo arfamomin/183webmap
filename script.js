ea2d15c910790fd7752afa40d80a7241b705f5bd
Arfa Momin <arfamomin16@gmail.com>
2025-10-27 09:54:03 -0700

    feat: add mapbox webmap

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJmYW1vbWluIiwiYSI6ImNscGwwYzZlMDAxaHgyanA1cWUzY2ExN3YifQ.z5jXdp6__K-B2oj1rpNOJw';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    center: [-122.27, 37.85],
+    zoom: 10,
+    style: 'mapbox://styles/arfamomin/cmh9cuq2f00px01r5h1e5e1ss',
+})
